25639714fa4fdb46dc33c2b0eca3488ab419df36
Zahit Egemen İm <zegemenim@gmail.com>
2024-08-07 16:39:26 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import FooterComponent from "@/Components/FooterComponent";
+import HeaderComponent from "@/Components/HeaderComponent";
 
 export default function Home() {
   return (
     <main className="">
-      <FooterComponent />
+      <HeaderComponent />
 
 
     </main>
