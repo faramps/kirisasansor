b1b0d0e2065e0212e83d6fd89a4650060a127799
Zahit Egemen İm <zegemenim@gmail.com>
2024-08-07 16:51:27 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-
+        <HeaderComponent />
       {children}
       </body>
     </html>
