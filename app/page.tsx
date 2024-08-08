945dd40fa226a91988518b950998f3b1f7b954f7
Zahit Egemen İm <zegemenim@gmail.com>
2024-08-09 02:25:47 +0300

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import hakkimizda from "@/public/asansor.jpg";
 import React from "react";
 import Link from "next/link";
-import $ from 'jquery';
+import $ from "jquery";
 import {useEffect} from "react";
 
 export default function Home() {
